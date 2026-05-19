58c6dbda17556d83d7f4ff3842ccd76dd752e72e
Bruno <brunorosabrs1234@gmail.com>
2026-05-19 17:43:48 -0300

    Add files via upload

---
+++ exerciciosResolvidos.js
@@ -0,0 +1,58 @@
+const ask = require('readline-sync');
+
+/*/ EXERCICIO 1 
+
+let name = 'Bruno';
+let age = '29';
+const PI = 3.14
+console.log(`Nome: ${name}\nIdade: ${age}\nPI: ${PI}`);
+
+*/
+
+/*/ EXERCICIO 2
+
+let num1 = Number(ask.question('Enter the first number: '));
+let num2 = Number(ask.question('Enter the second number: '));
+console.log(`sum: ${num1 + num2}\nsubtraction: ${num1 - num2}\nmultiplication: ${num1 * num2}\ndivision: ${num1 / num2}\nrest of division: ${num1 % num2}`);
+*/
+
+/*/ EXERCICIO 3
+
+let num1 = 50;
+let num2 = 60;
+
+let equality = num1 === num2;
+let difference = num1 !== num2;
+let greaterThan = num1 > num2;
+let lessThan = num1 < num2;
+let greaterThanOrEqualTo = num1 >= num2;
+let lessThanOrEqualTo = num1 <= num2;
+
+console.log(`${equality}\n${difference}\n${greaterThan}\n${lessThan}\n${greaterThanOrEqualTo}\n${lessThanOrEqualTo}`);
+*/
+
+/*/ EXERCICIO 4 
+
+function canDrive(age, driversLicence) {
+    if (age >= 18 && driversLicence.toLowerCase() === 'sim') {
+        return 'Voce pode dirigir'
+
+    } else {
+        return 'Não pode dirigir'
+    }
+}
+
+
+let age = Number(ask.question('Digite sua idade: '));
+let driversLicence = ask.question('Voce possui carteira de motorista? (sim / nao): ');
+console.log(canDrive(age, driversLicence));
+*/
+
+// EXERCICIO 5
+
+
+
+
+
+
+
